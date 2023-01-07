c589b8c66172061dc9ea7db1a282392d3205cfa3
manas887 <116465782+manas887@users.noreply.github.com>
2023-01-07 13:46:55 +0530

    completed

--- script.js
+++ script.js
@@ -8,20 +8,52 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map((element) => {
+    if (element.profession == "developer")
+      console.log(element);
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((element) => {
+    if (element.profession == "developer")
+      console.log(element);
+  })
 }
+
 
 function addData() {
   //Write your code here
+  const newlist = {
+    id: 4,
+    name: "susan",
+    age: "20",
+    profession: "intern"
+  }
+  // arr = [...arr,newlist]
+  arr.push(newlist)
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  arr.filter((element) => {
+    if (element.profession != "admin")
+      console.log(element);
+  })
+
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [{ id: 5, name: "mohan", age: "17", profession: "developer" },
+  { id: 6, name: "jaz", age: "25", profession: "itern" },
+  { id: 7, name: "kiran", age: "29", profession: "developer" }]
+
+  arr = [...arr, ...arr2]
+  console.log(arr);
+  // let arr3 = arr.concat(arr2);
+  // console.log(arr3)
 }
+
